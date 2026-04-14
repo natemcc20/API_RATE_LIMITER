d76b29d5946855f027ac87caec35937665f5915b
Nate <natemcc30@gmail.com>
2026-04-13 20:53:25 -0700

    Comment changes

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ MAX_REQUESTS = 10    #max requests per window (sliding window)
 BUCKET_CAPACITY = 10          #max tokens
 REFILL_RATE = 10 / 60         #tokens per second 10 per min
 
-ALGORITHM = 'sliding_window'  #sliding_window or token_bucket
+ALGORITHM = 'token_bucket'  #sliding_window or token_bucket
 
 #Space
 request_log = defaultdict(list)   #timestamps for each IP
@@ -23,9 +23,9 @@ buckets = {}                       #token bucket, state per IP
 #Sliding window technique, interchangeable
 def is_rate_limited_sliding(ip):
     now = time.time()
-    window_start = now - WINDOW
+    window_start = now - WINDOW #Since we used time(), its UNIX, time since midnight Jan 1 1970, easiest for tracking
 
-    request_log[ip] = [t for t in request_log[ip] if t > window_start]
+    request_log[ip] = [t for t in request_log[ip] if t > window_start] #trash old reqs
 
     if len(request_log[ip]) >= MAX_REQUESTS:
         return True
@@ -45,9 +45,9 @@ def is_rate_limited_bucket(ip):
     bucket = buckets[ip]
 
     #Refill tokens
-    elapsed = now - bucket['last_refill']
-    bucket['tokens'] = min(BUCKET_CAPACITY, bucket['tokens'] + elapsed * REFILL_RATE)
-    bucket['last_refill'] = now
+    elapsed = now - bucket['last_refill']   #seconds since last refill,if 6 / elapes = 6
+    bucket['tokens'] = min(BUCKET_CAPACITY, bucket['tokens'] + elapsed * REFILL_RATE) #refill tokens based on elapsed time, cap at max capacity
+    bucket['last_refill'] = now  #resets refill clock 
 
     if bucket['tokens'] < 1:
         return True
